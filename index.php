0dc1dd0a3e9a6e6140a064e4e591a41601dcab00
Wara Massa <waramassa70@gmail.com>
2020-11-24 00:45:59 +0100

    php exo1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// déclaration de la varable objet et initialisation avec la valeur  "l'objet du message est mystèrieux
+$objet = 'l\objet du message est mystèrieux...';
+// déclaration et initialisation de la variable
+$message = 'la valeur de ce messageest mystèrieux mais vous n\'en saurez pas plus';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP exo1</title>
+</head>
+<body>
+    <p><?php echo $objet; ?></p>
+    <p><?php echo $message; ?></p>
+   
+
+    
+</body>
+</html>
